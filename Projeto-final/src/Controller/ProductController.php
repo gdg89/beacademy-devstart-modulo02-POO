593fc901892011e36e5e84fc3e87e1ae2e719b35
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 12:31:23 -0300

    create ProductController

---
+++ Projeto-final/src/Controller/ProductController.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+class ProductController
+{
+    public function productAction(): void{
+        echo 'Ola mundo, estou deno de uma action, dentro de um controller';
+    }
+}
