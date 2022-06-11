5fec882e37143173cc3b449b78db91f2ca92cdb0
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 12:15:11 -0300

    src/controller

---
+++ Projeto-final/src/Controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+declare (strict_types=1);
+
+class IndexController
+{
+    //metodo
+    public function indexAction(): void {
+        echo 'Ola mundo, estou deno de uma action, dentro de um controller';
+    }
+}
