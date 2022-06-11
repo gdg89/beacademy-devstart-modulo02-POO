f1e479483232e04093d23a446ae5d7de8d0ae50f
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 12:30:20 -0300

    create CategoryController

---
+++ Projeto-final/src/Controller/CategoryController.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+class CategoryController
+{
+    public function CategoryAction(){
+        echo 'Ola mundo, estou dentro de uma action, dentro de um controller';
+    }
+}
