73f93aa730b89f723387845b495242ba50951492
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 22:23:05 -0300

    instantiate ProductController and CategoryController

--- Projeto-final/public/index.php
+++ Projeto-final/public/index.php
@@ -4,13 +4,20 @@
 
     use App\Controller\IndexController;
     use App\Controller\ProductController;
+    use App\Controller\CategoryController;
 
-    $c = new IndexController();//instanciando indexControler.
-    //$c->indexAction();
-    $c->loginAction();
+    $i = new IndexController();//instanciando indexControler.
+    $i->indexAction();
+    $i->loginAction();
 
     $p = new ProductController();
     $p->listAction();
     $p->addAction();
     $p->editAction();
+
+    $c = new CategoryController();
+    $c->listAction();
+    $c->addAction();
+    $c->editAction();
+
 
--- Projeto-final/src/Controller/ProductController.php
+++ Projeto-final/src/Controller/ProductController.php
@@ -2,24 +2,25 @@
 declare(strict_types=1);
 
 namespace App\Controller;// rota do namespace
+use App\Controller\AbstractController;
 
 //Classe
 class ProductController extends AbstractController
 { 
     //metdos
-    public function listAction()
+    public function listAction(): void
     {
         //include 'src/View/product/list.php';//incluindo rota da view - listar produto
         parent::render('product/list');
     }
 
-    public function addAction()
+    public function addAction(): void
     {
         //include 'src/View/product/add.php';// cadastrar produto
         parent::render('product/add');
     }
 
-    public function editAction()
+    public function editAction(): void
     {
         //include 'src/View/product/edit.php';// editar produto
         parent::render('product/edit');
