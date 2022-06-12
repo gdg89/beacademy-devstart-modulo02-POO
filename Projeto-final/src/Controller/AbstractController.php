c7f1c306ac4b758c042c16d854f4ba74c138c21f
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 21:27:36 -0300

    create class AbstractController

---
+++ Projeto-final/src/Controller/AbstractController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+abstract class AbstractController
+{
+    public function render(string $viewName): void      // renderizando html
+    {   
+        include 'src/View/{$viewname}.php';
+    }
+}
