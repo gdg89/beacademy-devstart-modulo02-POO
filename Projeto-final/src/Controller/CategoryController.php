ccd43a4c8e9912dda99c52ce98562d95aa42e559
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-11 21:59:56 -0300

    add AbstractController in CategoryCotroller

--- Projeto-final/src/Controller/CategoryController.php
+++ Projeto-final/src/Controller/CategoryController.php
@@ -2,7 +2,21 @@
 declare(strict_types=1);
 
 namespace App\Controller;
-class CategoryController
+
+use App\Controller\AbstractController;
+
+
+class CategoryController extends AbstractController
 {
-    
+  public function listAction(): void{
+    parent::render('Category/list');
+  }  
+
+  public function addAction(): void{
+    parent::render('Category/add');
+  }
+
+  public function editAction(): void{
+    parent::render('Category/edit');
+  }
 }
